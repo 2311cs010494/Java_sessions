5dc2835758bb4d372f5d9ec7a48defe1eb816270
2311cs010494 <vinodnethikoppula@gmail.com>
2025-01-07 16:51:39 +0530

    Add files via upload

---
+++ day5Loops.java
@@ -0,0 +1,10 @@
+public class day5Loops {
+        public static void main(String[] args) {
+        int i;
+            for( i = 11; i<=20; i++);
+            if(i % 2 == 0){
+            System.out.println(i);
+        }
+    }
+     
+}
