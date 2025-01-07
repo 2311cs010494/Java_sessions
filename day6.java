8ff35df5f9ffb618143aaa28d0fb613b8cf0c915
2311cs010494 <vinodnethikoppula@gmail.com>
2025-01-07 16:50:35 +0530

    Add files via upload
    Java program

---
+++ day6.java
@@ -0,0 +1,12 @@
+public class day6 {
+        public static void main(String args[]){
+            String username = "Vinod";
+            String pwd ="Vinod_123";
+            while((username =="Vinod")&&(pwd=="Vinod_123")){
+                System.out.println("Hello Vinod welcome to Malla Reddy university");
+                break;
+            }
+        }
+    }
+    
+    
